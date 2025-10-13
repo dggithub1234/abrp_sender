4752858ba59013e48643ff81ecbd0ddf9f09f61c
Daniel G <154083145+dggithub1234@users.noreply.github.com>
2025-10-13 09:08:44 +0100

    Update __init__.py

--- custom_components/__init__.py
+++ custom_components/__init__.py
@@ -1,9 +1,8 @@
-""""Initialize the ABRP Sender integration."""
+"""Initialize the ABRP Sender integration."""
 from __future__ import annotations
 import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import config_entry_oauth2_flow
 
 from .const import DOMAIN
 from .coordinator import ABRPSenderCoordinator
@@ -16,8 +15,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     coordinator = ABRPSenderCoordinator(hass, entry)
     await coordinator.async_setup()
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
-
-    # Reload on option changes
     entry.async_on_unload(entry.add_update_listener(async_reload_entry))
     return True
 
@@ -33,4 +30,3 @@ async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Reload integration when options change."""
     await async_unload_entry(hass, entry)
     await async_setup_entry(hass, entry)
-
