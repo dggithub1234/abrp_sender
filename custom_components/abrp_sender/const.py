249ce4b65db2bf935e53980dca2e53d7a5b463fe
Daniel G <154083145+dggithub1234@users.noreply.github.com>
2025-10-13 12:16:35 +0100

    Update const.py

--- custom_components/abrp_sender/const.py
+++ custom_components/abrp_sender/const.py
@@ -1,4 +1,11 @@
+"""Constants for ABRP Sender integration."""
+
 DOMAIN = "abrp_sender"
 DEFAULT_SCAN_INTERVAL = 60
 ABRP_API_URL = "https://api.iternio.com/1/tlm/send"
+
+# ABRP API key constant
+ABRP_API_KEY = "1e28ad14-df16-49f0-97da-364c9154b44a"
+
 CONF_ENABLED = "enabled"
+CONF_API_TOKEN = "api_token"
